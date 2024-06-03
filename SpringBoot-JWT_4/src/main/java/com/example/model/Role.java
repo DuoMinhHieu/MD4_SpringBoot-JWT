552c8c95536415be732050ec609e53534c2f0472
DuoMinhHieu <dmh6774@gmail.com>
2024-06-03 15:07:58 +0700

    bai 3

--- SpringBoot-JWT_4/src/main/java/com/example/model/Role.java
+++ SpringBoot-JWT_4/src/main/java/com/example/model/Role.java
@@ -1,2 +1,17 @@
-package com.example.model;public class Role {
+package com.example.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
 }
+
--- SpringBoot-JWT_4/src/main/java/com/example/repo/IUserRepository.java
+++ SpringBoot-JWT_4/src/main/java/com/example/repo/IUserRepository.java
@@ -1,2 +1,10 @@
-package com.example.repo;public interface IUserRepository {
+package com.example.repo;
+
+import com.example.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
